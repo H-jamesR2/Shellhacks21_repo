3d4a61b363388d90e5a37d4eb5144aab9726f1a6
Jamswhat2 <hilarionjames25@gmail.com>
2021-09-25 14:06:37 -0400

    updated train.py and notes

--- train.py
+++ train.py
@@ -11,3 +11,20 @@ tags = []
 xy = []
 #training data from json file...
 # function_loops:
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append(w, tag)
+
+ignore_words = ['?','!','.',',']
+all_words = [stem(w) fro w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+X_train = [] 
+Y_train = []
+for (pattern_sentence, tag) in xy:
+
